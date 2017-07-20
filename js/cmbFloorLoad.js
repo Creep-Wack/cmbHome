d5d808767907afd199eaaf61f4455191316529a7
Wack- <weirdoPro@163.com>
2017-07-20 17:49:32 +0800

    2017-7-20 17:47:17
    1、已完成1148/14/113模块拼装
    2、确定模块ID（1-横幅模块、2-1X3模块、3-1X4模块、4-1X8A模块、5-1X8B模块、6-1X1X3模块、7-1X1X4X8模块）
    3、已发现问题：
       i、最下面一块迟迟加载不完整

--- js/cmbFloorLoad.js
+++ js/cmbFloorLoad.js
@@ -1,8 +1,8 @@
 var Floor = new Object();
-Floor.SysnoCont = new Array();//各模块ID
-Floor.TypeCont = new Array();//各模块ID
-Floor.blockCont = new Array();//各楼层对象
-Floor.loadFloor = function(_url){//装载顶部导航
+Floor.SysnoCont = new Array();//保存模块ID指针
+Floor.TypeCont = new Array();//保存模块类型指针
+Floor.blockCont = new Array();//保存各楼层对象
+Floor.loadFloor = function(_url){//装载楼层
 	$.ajax({
 		type:'GET',
 		url:_url,
@@ -11,7 +11,7 @@ Floor.loadFloor = function(_url){//装载顶部导航
 			console.log('error'+arguments[1]);
 		},
 		success:function(data){
-			var content;
+			var content="";
 			$.each(data,function(ind,obj){//遍历原始JSON对象
 				if(Floor.SysnoCont.indexOf(obj.ModelSysno)==-1){//新模块则新建一个数组存放模块内容
 					Floor.SysnoCont.push(obj.ModelSysno);
@@ -21,7 +21,15 @@ Floor.loadFloor = function(_url){//装载顶部导航
 				Floor.blockCont[Floor.blockCont.length-1].push(obj);	
 			});
 			$.each(Floor.blockCont,function(key,value){
-				Floor.loadBlock(Floor.TypeCont[key],value);
+				content += Floor.loadBlock(Floor.TypeCont[key],value);
+			});
+			$('.index-page').append(content);
+			h360Swiper = new Swiper('.common-h360-banner',{
+				autoplay:3000,
+				pagination : '.common-pagination',
+				loop:true,
+				observer:true,
+				autoplayDisableOnInteraction:false
 			});
 		}
 	});		
@@ -30,31 +38,14 @@ Floor.loadBlock = function(style,obj){//页面组装
 	var _html = "<section class=\"container\">";
 	var counter=0;//当前对象内部计数器
 	var lastPosition=0;//上一个对象类型
+	// console.log(obj);
 	var tailHtml="";
 	if(style==1148){//1X1X4X8模块 position:1-楼层标题  2-360高度通栏轮播  3-540X280高度一行两列广告位  4-270X360高度一行四列广告位
 
 		$.each(obj,function(k,v){
-			// if(k==obj.length-1){//是否是最后一个对象
-			// 	console.log(lastPosition);
-			// 	swith(lastPosition)
-			// 	{
-			// 		case 0:
-			// 			tailHtml="";
-			// 		 	break;
-			// 		case 1:
-			// 			tailHtml="";
-			// 		 	break;
-			// 		case 2:
-			// 			tailHtml="</div><div class=\"common-pagination\"></div></div></section>";
-			// 			break;
-			// 		case 3:
-			// 			tailHtml="</div></section>";
-			// 		case 4:
-			// 			tailHtml="</div></section>";
-			// 	}
-			// }
-			// console.log(v);
-			if(lastPosition!=0&&lastPosition!=1&&lastPosition!=v.Position){//上一步组装元素与当前组装对象非同类型
+			tailHtml = Floor.addTail(style,lastPosition,k,obj);
+			// console.log(lastPosition+'||'+v.Position);
+			if(lastPosition!=v.Position){//上一步组装元素与当前组装对象非同类型
 				counter=0;
 				if(lastPosition==2){
 					_html+="</div><div class=\"common-pagination\"></div></div>";
@@ -64,49 +55,144 @@ Floor.loadBlock = function(style,obj){//页面组装
 				}
 			}
 			if(v.Position==1){//楼层标题
-				lastPosition=1;
+				lastPosition=v.Position;
 				_html+= "<div class=\"block-title h120\"><img src=\""+v.ResourceUrl+"\" alt=\"楼层标题\"></div>";
 			}
 			else if(v.Position==2){//360高度轮播
-				lastPosition=2;
-				if(!counter){//第一张广告
-					counter++;
+				lastPosition=v.Position;
+				if(counter==0){//第一张广告
 					_html += "<div class=\"swiper-container common-h360-banner container\"><div class=\"swiper-wrapper\"><div class=\"swiper-slide\"><a href=\""+v.Link+"\"><img src=\"https://img01.mall.cmbchina.com/banner/default.jpg\" data-original=\""+v.ResourceUrl+"\"></a></div>";
 				}
-				else if(0<counter<=5){//限制广告数量五张以内
-					counter++;
+				else if(0<counter<=4){//限制广告数量五张以内
 					_html += "<div class=\"swiper-slide\"><a href=\""+v.Link+"\"><img src=\"https://img01.mall.cmbchina.com/banner/default.jpg\" data-original=\""+v.ResourceUrl+"\"></a></div>";
+				}
+				else{
+					_html+="";
 				}
 			}
 			else if(v.Position==3){
-				lastPosition=3;
+				lastPosition=v.Position;
 				
-				if(!counter){//第一张540X280高广告位
-					counter++;
+				if(counter==0){//第一张540X280高广告位
 					_html += "<div class=\"h280 columns col2 clearfix\"><a href=\""+v.Link+"\"><img src=\"https://img01.mall.cmbchina.com/banner/default.jpg\" data-original=\""+v.ResourceUrl+"\"></a>";
 				}
-				else if(0<counter<=4){//限制广告数量4张以内
-					counter++;
+				else if(0<counter&&counter<4){//限制广告数量4张以内
 					_html += "<a href=\""+v.Link+"\"><img src=\"https://img01.mall.cmbchina.com/banner/default.jpg\" data-original=\""+v.ResourceUrl+"\"></a>";
+				}
+				else{
+					_html+="";
 				}
 			}
 			else if(v.Position==4){
-				lastPosition=4;
+				lastPosition=v.Position;
 				
 				if(!counter){//第一张270X360高广告位
-					counter++;
 					_html += "<div class=\"h360 columns col4 clearfix\"><a href=\""+v.Link+"\"><img src=\"https://img01.mall.cmbchina.com/banner/default.jpg\" data-original=\""+v.ResourceUrl+"\"></a>";
 				}
-				else if(0<counter<=8){//限制广告数量8张以内
-					counter++;
+				else if(0<counter&&counter<=7){//限制广告数量8张以内
 					_html += "<a href=\""+v.Link+"\"><img src=\"https://img01.mall.cmbchina.com/banner/default.jpg\" data-original=\""+v.ResourceUrl+"\"></a>";
 				}
 			}
+			counter++;
 			
 		});
-		_html+=tailHtml;
-		tailHtml="";
-		console.log(_html);
+
+		
+	}
+	else if(style==14){//1X4模块  1-楼层标题  2-540X360 一行两列广告模块
+		$.each(obj,function(k,v){
+			tailHtml = Floor.addTail(style,lastPosition,k,obj);
+			if(lastPosition!=v.Position){//上一步组装元素与当前组装对象非同类型
+				counter=0;
+			}
+			if(v.Position==1){//楼层标题
+				lastPosition=v.Position;
+				_html+= "<div class=\"block-title h120\"><img src=\""+v.ResourceUrl+"\" alt=\"楼层标题\"></div>";
+			}
+			else if(v.Position==2){//540X360高度
+				lastPosition=v.Position;
+				if(counter==0){//第一张广告
+					_html += "<div class=\"h360 columns col2 clearfix\"><a href=\""+v.Link+"\"><img src=\"https://img01.mall.cmbchina.com/banner/default.jpg\" data-original=\""+v.ResourceUrl+"\"></a>";
+				}
+				else if(0<counter<=3){//限制广告数量4张以内
+					_html += "<a href=\""+v.Link+"\"><img src=\"https://img01.mall.cmbchina.com/banner/default.jpg\" data-original=\""+v.ResourceUrl+"\"></a>";
+				}
+				else{
+					_html+="";
+				}
+
+			}
+			counter++;
+		});
+	}
+	else if(style==113){//1X1X3模块  1-120楼层标题  1-360高度通栏横幅 3-商品模块
+		$.each(obj,function(k,v){
+			tailHtml = Floor.addTail(style,lastPosition,k,obj);
+			if(lastPosition!=v.Position){//上一步组装元素与当前组装对象非同类型
+				counter=0;
+			}
+			if(v.Position==1){//楼层标题
+				lastPosition=v.Position;
+				_html+= "<div class=\"block-title h120\"><img src=\""+v.ResourceUrl+"\" alt=\"楼层标题\"></div>";
+			}
+			else if(v.Position==2){//540X360高度
+				lastPosition=v.Position;
+				if(counter==0){//第一张广告
+					_html += "<div class=\"h360 columns col2 clearfix\"><a href=\""+v.Link+"\"><img src=\"https://img01.mall.cmbchina.com/banner/default.jpg\" data-original=\""+v.ResourceUrl+"\"></a>";
+				}
+				else if(0<counter<=3){//限制广告数量4张以内
+					_html += "<a href=\""+v.Link+"\"><img src=\"https://img01.mall.cmbchina.com/banner/default.jpg\" data-original=\""+v.ResourceUrl+"\"></a>";
+				}
+				else{
+					_html+="";
+				}
+
+			}
+			counter++;
+		});
+	}
+	else{
+		return '';
+	}
+
+	_html+=tailHtml+"</section>";
+	tailHtml="";
+	return _html;
+
+}
+Floor.addTail = function(sty,lastpos,ind,o){//处理模块尾部
+	if(ind==o.length-1){
+		if(sty==1148){
+			switch (lastpos)
+				{	
+					case 0:
+					case 1:
+						return "";
+						break;
+					case 2:
+						return "</div><div class=\"common-pagination\"></div></div>";
+						break;
+					case 3:
+					case 4:
+						return "</div>";
+						break;
+				}
+		}
+		else if(sty==14){
+			switch (lastpos)
+				{
+					case 0:
+					case 1:
+						return "";
+					 	break;
+					case 2:
+						return "</div>";
+						break;
+				}
+		}
+		else{
+			return "";
+		}
 	}
 
 }
